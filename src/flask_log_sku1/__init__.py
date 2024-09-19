b4365bc99ff789bd4421aa9154f86001d8156ba1
Skulldorom <51134009+Skulldorom@users.noreply.github.com>
2024-09-19 11:52:30 +0300

    Fix

--- src/flask_log_sku1/__init__.py
+++ src/flask_log_sku1/__init__.py
@@ -10,7 +10,7 @@ class bcolors:
     OKBLUE = '\033[34m'
     OKCYAN = '\033[36m'
     OKGREEN = '\033[32m'
-    WARNING = '\033[33m'.
+    WARNING = '\033[33m'
     FAIL = '\033[31m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
